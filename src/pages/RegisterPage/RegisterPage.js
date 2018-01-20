fa8d7b226b28cd910ded1c27bf33cfb5d90126bc
Kevin Romero Peces-Barba <kevinrpb@hotmail.com>
2018-01-20 01:53:31 -0800

    Design for register and login pages completed. Added Makefile.

--- src/pages/RegisterPage/RegisterPage.js
+++ src/pages/RegisterPage/RegisterPage.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Navbar from '../../components/Navbar/Navbar';
 import Footer from '../../components/Footer/Footer';
 import './RegisterPage.css'
+
+function RegisterForm() {
+	return (
+		<form>
+			<h2>Join awesomeness</h2>
+			<div className="form-group">
+				<label htmlFor="register-input-email">Email</label>
+				<input type="email" className="form-control form-control-lg" id="register-input-email" placeholder="john@lennon.org" />
+			</div>
+			<div className="form-group">
+				<label htmlFor="register-input-name">Username</label>
+				<input type="text" className="form-control form-control-lg" id="register-input-name" placeholder="JLennon" />
+			</div>
+			<div className="form-group">
+				<label htmlFor="register-input-pwd">Password</label>
+				<input type="password" className="form-control form-control-lg" id="register-input-pwd" placeholder="********" />
+			</div>
+			<button className="btn btn-lg btn-block btn-primary">Sign Up</button>
+			<p>Already have an account? <Link to="/login">Login</Link></p>
+		</form>
+	);
+}
 
 class RegisterPage extends React.Component {
 
@@ -9,6 +32,15 @@ class RegisterPage extends React.Component {
 		return (
 			<div>
 				<Navbar menuItems={[["Home", false, "/"], ["Explore", false, "/explore"], ["Create", false, "/create"]]}/>
+				<div className="container">
+					<div className="row align-items-center">
+						<div className="col-s-12 col-md-8 offset-md-2 col-lg-6 offset-lg-3 align-self-center">
+							<div className="jumbotron wow slideInRight" id="register-form-container">
+								<RegisterForm />
+							</div>
+						</div>
+					</div>
+				</div>
 				<Footer />
 			</div>
 		);
