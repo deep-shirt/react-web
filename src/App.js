7a5aca419da39fb7a7a58471d16d108a3357efd9
James Huang <jameshuanguc@gmail.com>
2018-01-20 14:43:24 -0800

    changed routes format

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
 
 import HomePage from './pages/HomePage/HomePage';
 import ExplorePage from './pages/ExplorePage/ExplorePage';
@@ -9,18 +9,22 @@ import LoginPage from './pages/LoginPage/LoginPage';
 import RegisterPage from './pages/RegisterPage/RegisterPage';
 import DetailPage from './pages/DetailPage/DetailPage';
 
+import * as routes from './constants/routes';
+
 class App extends React.Component {
   render() {
     return (
+    	<Router>
 			<div>
-				<Route exact path="/" component={HomePage}/>
-				<Route exact path="/explore" component={ExplorePage}/>
-				<Route exact path="/create/pickcontent" component={PickContent}/>
-				<Route exact path="/create/pickstyle" component={PickStylePage}/>
-				<Route exact path="/login" component={LoginPage}/>
-				<Route exact path="/register" component={RegisterPage}/>
-				<Route exact path="/designs/:id" component={DetailPage}/>
+				<Route exact path={routes.HOMEPAGE} component={HomePage}/>
+				<Route exact path={routes.EXPLOREPAGE} component={ExplorePage}/>
+				<Route exact path={routes.PICKCONTENTPAGE} component={PickContent}/>
+				<Route exact path={routes.PICKSTYLEPAGE} component={PickStylePage}/>
+				<Route exact path={routes.LOGINPAGE} component={LoginPage}/>
+				<Route exact path={routes.REGISTERPAGE} component={RegisterPage}/>
+				<Route exact path={routes.DETAILPAGE} component={DetailPage}/>
 			</div>
+		</Router>
     );
   }
 }
---
+++ src/constants/routes.js
@@ -0,0 +1,8 @@
+export const LANDINGPAGE = '/';
+export const HOMEPAGE = '/';
+export const EXPLOREPAGE = '/explore';
+export const PICKCONTENTPAGE = '/create/pickcontent';
+export const PICKSTYLEPAGE = '/create/pickstyle';
+export const LOGINPAGE = '/login';
+export const REGISTERPAGE = '/register';
+export const DETAILPAGE = '/designs/:id';
