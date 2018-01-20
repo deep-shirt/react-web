164bca5f424e2045ba2aa08782d5b6740e4b6cb7
philipp <philippgrulich@hotmail.de>
2018-01-20 05:56:18 -0800

    createPage

--- src/pages/CreatePage/CreatePage.js
+++ src/pages/CreatePage/CreatePage.js
@@ -2,16 +2,22 @@ import React from 'react';
 import Navbar from '../../components/Navbar/Navbar';
 import Footer from '../../components/Footer/Footer';
 import './CreatePage.css'
+import { fadeIn } from 'react-animations'
 
 class CreatePage extends React.Component {
 	constructor(props) {
 		super(props);		
 		this.pickcontent = this.pickcontent.bind(this);
 		this.pickstyle = this.pickstyle.bind(this);
+		this.buttonClicked = this.buttonClicked.bind(this);
 		this.state = {
 			contentImage : undefined,
 			styleImage : undefined,
 		}
+	}	
+
+	buttonClicked(e){
+		var contentPickerElement = this.refs.contentPicker;
 	}	
 
 	pickcontent(image){
@@ -32,63 +38,68 @@ class CreatePage extends React.Component {
 				<Navbar menuItems={[["Home", false, "/"], ["Explore", false, "/explore"], ["Create", true, "/create"]]}/>
 				<div className="container createPage">
 					<h1 className="display-4">Style creator:</h1>
-					<p className="lead">
-						Upload the image you want to style.
-					</p>
-					<div className="row selectbox">				
-						<div className="col-sm">
-							<ul className="nav nav-tabs" role="tablist">
-							<li className="nav-item">
-								<a className="nav-link active" data-toggle="tab" href="#home" role="tab">Upload own picture</a>
-							</li>
-							<li className="nav-item">
-								<a className="nav-link" data-toggle="tab" href="#profile" role="tab">Select Flickr image</a>
-							</li>
-							</ul>
-							<div className="tab-content">
-								<div className="tab-pane active" id="home" role="tabpanel">
-									<ImageUpload callback={this.pickcontent}></ImageUpload>
-								</div>
-								<div className="tab-pane" id="profile" role="tabpanel">Flickr
-								</div>
-							</div>		
-						</div>	
-						<div className="col">
-							Content image:
-							<ImageThumbnail imagefile={this.state.contentImage}></ImageThumbnail>
+					<div className="contentPicker wow slideInRight" ref="contentPicker">
+						<p className="lead">
+							Upload the image you want to style.
+						</p>
+						<div className="row selectbox">				
+							<div className="col-sm">
+								<ul className="nav nav-tabs" role="tablist">
+								<li className="nav-item">
+									<a className="nav-link active" data-toggle="tab" href="#home" role="tab">Upload own picture</a>
+								</li>
+								<li className="nav-item">
+									<a className="nav-link" data-toggle="tab" href="#profile" role="tab">Select Flickr image</a>
+								</li>
+								</ul>
+								<div className="tab-content">
+									<div className="tab-pane active" id="home" role="tabpanel">
+										<ImageUpload callback={this.pickcontent}></ImageUpload>
+									</div>
+									<div className="tab-pane" id="profile" role="tabpanel">Flickr
+									</div>
+								</div>		
+							</div>	
+							<div className="col">
+								Content image:
+								<ImageThumbnail imagefile={this.state.contentImage}></ImageThumbnail>
+							</div>
 						</div>
 					</div>
-					<p className="lead">
-						Select the style 
-					</p>
-					<div className="row selectbox">				
-						<div className="col-sm">
-							<ul className="nav nav-tabs" role="tablist-style">
-							<li className="nav-item">
-								<a className="nav-link active" data-toggle="tab" href="#pre-style" role="tab">Predefined styles</a>
-							</li>
-							<li className="nav-item">
-								<a className="nav-link" data-toggle="tab" href="#user-style" role="tab">Upload own style</a>
-							</li>
-							</ul>
-							<div className="tab-content">
-								<div className="tab-pane active" id="pre-style" role="tabpanel">
-								
-								</div>
-								<div className="tab-pane" id="user-style" role="tabpanel">
-								
-									<ImageUpload callback={this.pickstyle}></ImageUpload>
-								</div>
-							</div>		
-						</div>	
-						<div className="col">
-								Style image:
-								<ImageThumbnail imagefile={this.state.styleImage}></ImageThumbnail>
+					<div className="stylePicker wow slideInRight">
+						<p className="lead">
+							Select the style 
+						</p>
+						<div className="row selectbox">				
+							<div className="col-sm">
+								<ul className="nav nav-tabs" role="tablist-style">
+								<li className="nav-item">
+									<a className="nav-link active" data-toggle="tab" href="#pre-style" role="tab">Predefined styles</a>
+								</li>
+								<li className="nav-item">
+									<a className="nav-link" data-toggle="tab" href="#user-style" role="tab">Upload own style</a>
+								</li>
+								</ul>
+								<div className="tab-content">
+									<div className="tab-pane active" id="pre-style" role="tabpanel">
+									
+									</div>
+									<div className="tab-pane" id="user-style" role="tabpanel">
+									
+										<ImageUpload callback={this.pickstyle}></ImageUpload>
+									</div>
+								</div>		
+							</div>	
+							<div className="col">
+									Style image:
+									<ImageThumbnail imagefile={this.state.styleImage}></ImageThumbnail>
+							</div>
 						</div>
 					</div>
+					
 					<div className="row">	
 						<div className="col generateBtn">
-						<button type="button" class="btn btn-success">Generate Shirt Style</button>
+						<button type="button" class="btn btn-success"  onClick={this.buttonClicked}>Generate Shirt Style</button>
 						</div>
 					</div>
 				</div>
