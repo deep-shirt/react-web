147a0855870555fafcff99d6c7191b24e70595f6
philipp <philippgrulich@hotmail.de>
2018-01-20 00:59:27 -0800

    init push for create page

--- src/pages/CreatePage/CreatePage.js
+++ src/pages/CreatePage/CreatePage.js
@@ -9,6 +9,15 @@ class CreatePage extends React.Component {
 		return (
 			<div>
 				<Navbar menuItems={[["Home", false, "/"], ["Explore", false, "/explore"], ["Create", true, "/create"]]}/>
+				<div class="jumbotron">
+  					<h1 class="display-4">Hello, world!</h1>
+  					<p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+  					<hr class="my-4"></hr>
+  					<p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+  					<p class="lead">
+    					<a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+  					</p>
+				</div>
 				<Footer />
 			</div>
 		);
