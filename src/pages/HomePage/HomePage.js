598d80fb94050e69b2a5dc3d3110932c08608ab2
James Huang <jameshuanguc@gmail.com>
2018-01-21 09:56:57 -0800

    added link to detail

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -9,7 +9,9 @@ const GaleryItem = (props) => {
 	return (
 		<div className="col-12 col-md-4">
 			<div className="card wow fadeIn">
-				<img src={props.prev} alt="" className="card-img-top"/>
+				<Link to={props.path}>
+                    <img src={props.prev} alt="" className="card-img-top"/>
+                </Link>
 				<div className="card-body">
 					<h5 className="card-title">{props.name}</h5>
 					<h6 className="card-subtitle mb-2 text-muted">$ {props.price}</h6>
